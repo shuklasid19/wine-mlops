a6be14edc39af65ae45dadf54fc1638a79b5aabe
shuklasid19 <shuklasid19@gmail.com>
2021-11-04 19:42:31 +0530

    so tox abeen done

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="src", 
+    verion="0.0.1", 
+    description="its a mlops winrq package",
+    author="sid19", 
+    packages = find_packages(),
+    license = "MIT",
+
+
+)
